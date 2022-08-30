20fc129fbdc72d96669963159bf7853d1fb2709b
ansh13 <35523410+ansh13@users.noreply.github.com>
2022-08-30 23:31:05 +0530

    Update cloudgenix_settings.py

--- cloudgenix_settings.py
+++ cloudgenix_settings.py
@@ -2,7 +2,7 @@
 ### Additionally, scripts can take AUTH_TOKEN from X_AUTH_TOKEN or AUTH_TOKEN Environment Variables.
 ###
 
-CLOUDGENIX_USER = 'ansmittal@paloaltonetworks.com'
+CLOUDGENIX_USER = '1390anshmittal@gmail.com
 CLOUDGENIX_PASSWORD = 'PRacheta@123!4'
 
 ### Instead of CLOUDGENIX_USER or CLOUDGENIX_PASSWORD, you can set CLOUDGENIX_AUTH_TOKEN instead.
